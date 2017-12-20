3e68b96f14e5873ac79e6e432d3ae14b446b5010
Cody Mullins <joshua.c.mullins@gmail.com>
2017-12-20 11:22:07 -0500

    initial commit

---
+++ DemoClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace RefitDemoClient
+{
+    public interface IDemoClient
+    {
+        [Get("/demo")]
+        Task<HttpResponseMessage> Get();
+    }
+}
