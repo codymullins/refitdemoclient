7eaad1e9f94803f333c9e39d2c1b1ecb94c87d9a
Cody Mullins <joshua.c.mullins@gmail.com>
2017-12-20 11:32:00 -0500

    added class library example as well

---
+++ ClassLibraryDemo/IDemoClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace ClassLibraryDemo
+{
+    public interface IDemoClient
+    {
+        [Get("")]
+        Task<HttpResponseMessage> Get();
+    }
+}
